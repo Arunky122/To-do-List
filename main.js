2c40f7511b733651ec12a948ced07e119ecf643e
Arunky122 <arunky122@gmail.com>
2020-05-20 22:32:44 +0530

    add filter

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ var filterOption = document.querySelector('.filter-todo');
 //Event Listeners
 todoBtn.addEventListener('click', addList);
 todoList.addEventListener('click',deleteCheck);
-filterOption.addEventListener('click', filterList);
+filterOption.addEventListener('change', filterList);
 
 
 
@@ -36,45 +36,50 @@ function addList(e){
 
     
 
-    //create div in ul
-    const todoDiv = document.createElement('div');
-    todoDiv.classList.add('todo');
-    // todoDiv.classList.add('animate');
-    todoList.appendChild(todoDiv);
-    
+    let value = todoInput.value;
+    if(value){
+            //create div in ul
+        const todoDiv = document.createElement('div');
+        todoDiv.classList.add('todo');
+        // todoDiv.classList.add('animate');
+        todoList.appendChild(todoDiv);
+        
 
-    // Create list in the div
+        // Create list in the div
 
-    const todoItem = document.createElement('li');
-    todoItem.classList.add('item');
-    todoItem.innerHTML= todoInput.value;
-    // todoDiv.appendChild(todoItem);
-    todoDiv.appendChild(todoItem);
+        const todoItem = document.createElement('li');
+        todoItem.classList.add('item');
+        todoItem.innerHTML= todoInput.value;
+        // todoDiv.appendChild(todoItem);
+        todoDiv.appendChild(todoItem);
 
-    
-
-
-    // create completed button 
-
-    const comBtn = document.createElement('button');
-    comBtn.innerHTML = '<i class = "fas fa-check"></i>';
-    comBtn.classList.add('complete-btn');
-    todoDiv.appendChild(comBtn);
-
-    // create delete button 
-
-    const delBtn = document.createElement('button');
-    delBtn.innerHTML = '<i class = "fas fa-trash"></i>';
-    delBtn.classList.add('delete-btn');
-    todoDiv.appendChild(delBtn);
+        
 
 
-    // Attach this div to the ul
-    todoList.appendChild(todoDiv);
+        // create completed button 
 
-    //clear the input 
-    todoInput.value="";
+        const comBtn = document.createElement('button');
+        comBtn.innerHTML = '<i class = "fas fa-check"></i>';
+        comBtn.classList.add('complete-btn');
+        todoDiv.appendChild(comBtn);
 
+        // create delete button 
+
+        const delBtn = document.createElement('button');
+        delBtn.innerHTML = '<i class = "fas fa-trash"></i>';
+        delBtn.classList.add('delete-btn');
+        todoDiv.appendChild(delBtn);
+
+
+        // Attach this div to the ul
+        todoList.appendChild(todoDiv);
+
+        //clear the input 
+        todoInput.value="";
+
+        }else {
+            alert('Please type your to do ! ')
+        }
 
 }
 
@@ -103,6 +108,7 @@ function deleteCheck(e){
 // For filterList 
 
 function filterList(e){
+    
     // e.preventDefault();
 
     const todos = todoList.childNodes;
